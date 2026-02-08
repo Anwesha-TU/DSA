2bd1a152c8519d1862b726c5953b7b64f5304575
Anwesha Chakraborty <106398366+Anwesha-TU@users.noreply.github.com>
2026-02-08 10:01:25 +0530

    Time: 48 ms (47.74%) | Memory: 19.3 MB (35.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -18,13 +18,24 @@ class Solution:
         # return f(n)
 
         #Bottom up Tabulation: TC-O(n), SC-O(n)
+        # if n==0:
+        #     return 0
+        # if n==1:
+        #     return 1
+        # dp=[0]*(n+1)
+        # dp[0]=0
+        # dp[1]=1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[n]
+
+        #Constant Space solution- TC-O(n),SC-O(1)
         if n==0:
             return 0
         if n==1:
             return 1
-        dp=[0]*(n+1)
-        dp[0]=0
-        dp[1]=1
+        prev=0
+        curr=1
         for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+            prev,curr=curr,prev+curr
+        return curr
