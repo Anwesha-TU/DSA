a1fed27e3cc72984d6f906120750d8bfb57e3824
Anwesha Chakraborty <106398366+Anwesha-TU@users.noreply.github.com>
2026-02-08 09:43:12 +0530

    Time: 47 ms (51.12%) | Memory: 19.3 MB (11.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        # #recursive solution
+        # if n==0:
+        #     return 0
+        # elif n==1:
+        #     return 1
+        # else:
+        #     return self.fib(n-1)+self.fib(n-2)
+        #Top down memorization- O(n), SC-O(n)
+        memo={0:0,1:1}
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x]=f(x-1)+f(x-2)
+                return memo[x]
+        return f(n)
