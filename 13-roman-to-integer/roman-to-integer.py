d7ec8998fb6a0f60915fb518b22189759eaa6b7c
Anwesha Chakraborty <106398366+Anwesha-TU@users.noreply.github.com>
2026-03-07 16:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.49%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        summ=0
+        n=len(s)
+        for i in range(0,n):
+            if (i<n-1) and (dict[s[i]]<dict[s[i+1]]):
+                summ-=dict[s[i]]
+                # i+=2
+            else:
+                summ+=dict[s[i]]
+                i+=1
+        return summ
